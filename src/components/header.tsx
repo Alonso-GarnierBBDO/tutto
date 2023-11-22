c6ee8155aab64e5cc1104cc80df3f47bca3d89d4
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2023-11-22 11:12:41 -0600

    Se agrego el logo de tutto

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
-// import Image from "next/image";
+import Image from "next/image";
+import LogoTutto from '@/assets/img/tutto_logo.svg';
 
 /**
  * Desde aqui se maneja el header en todas las carpetas
@@ -11,7 +12,7 @@ const Header = () => {
     return (
         <>
             <header>
-                Hola mundo
+                <Image className="logo" src={LogoTutto.src} alt="Logo de tutto" width={100} height={100} />
             </header>
         </>
     )
