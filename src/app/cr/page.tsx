a7dd369322661b2215392ad42da17adf435771b2
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2023-11-22 10:30:00 -0600

    Se inicializo el proyecto

---
+++ src/app/cr/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/header';
+
+function CR(){
+
+    return (
+        <>
+            <Header/>
+        </>
+    )
+
+}
+
+export default CR;
---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+/**
+ * Desde aqui se maneja el header en todas las carpetas
+ * 
+ */
+
+
+const header = () => {
+
+    return (
+        <>
+            <header>
+                
+            </header>
+        </>
+    )
+
+}
+
+export default header;
