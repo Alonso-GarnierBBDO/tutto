26f663e438ac052fdfc91f64866d1831ada093c8
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2023-11-23 10:06:42 -0600

    Se agrego el texto del la inscription

--- src/app/cr/page.tsx
+++ src/app/cr/page.tsx
@@ -1,10 +1,11 @@
 import Header from '@/components/header';
-
+import InscriptionComponent from '@/components/inscription';
 function CR(){
 
     return (
         <>
             <Header/>
+            <InscriptionComponent/>
         </>
     )
 
---
+++ src/components/inscription.tsx
@@ -0,0 +1,18 @@
+
+
+const InscriptionComponent = () => {
+
+    return (
+        <>
+            <section className="content_inscription">
+                <section className="content">
+                    <p>¡Respondé mi formulario de los deseos y descubrí cuál de mis versiones no puede faltar en tu lista de Navidad!​</p>
+                    <p>Ahora solo completá las preguntas y participá por uno de los 10 kits con toda mi colección de temporada que te dejará sin aliento.</p>
+                </section>
+            </section>
+        </>
+    )
+
+}
+
+export default InscriptionComponent;
