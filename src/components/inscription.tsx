b72bc296a34e8d5e19baa37ab6b7d78a9f4db496
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2023-12-10 14:42:45 -0600

    Se agrego la opcion de las preguntas

--- src/components/inscription.tsx
+++ src/components/inscription.tsx
@@ -1,4 +1,5 @@
 import BuzonImage from '@/assets/img/buzon_two.png';
+import TrianguloImage from '@/assets/img/triangulo.png';
 import Image from 'next/image';
 
 type Content = {
@@ -21,6 +22,21 @@ const InscriptionComponent = ( { content } : Content ) => {
                         <p>{content.textOne}</p>
                         <p><strong>{content.textTwo}</strong></p>
                     </div>
+                </section>
+                <section className='premios'>
+                    <section className='menu'>
+                        <div>
+                            <hr />
+                            <Image src={TrianguloImage.src} width={TrianguloImage.width} height={TrianguloImage.height} alt="Triangulo Imagen"/>
+                        </div>
+                        <section>
+                            <h3>Ahora solo completá las preguntas y participá por:</h3>
+                        </section>
+                        <div>
+                            <Image src={TrianguloImage.src} width={TrianguloImage.width} height={TrianguloImage.height} alt="Triangulo Imagen"/>
+                            <hr />
+                        </div>
+                    </section>
                 </section>
             </section>
         </>
